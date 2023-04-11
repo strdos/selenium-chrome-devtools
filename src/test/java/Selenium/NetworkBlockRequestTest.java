7af4fac2fa3ae32a4a54635608732a90feb8847a
strdos <81235866+strdos@users.noreply.github.com>
2023-04-11 21:22:32 +0200

    add blocking unwanted requests (Network.setBlockedURLs)

---
+++ src/test/java/Selenium/NetworkBlockRequestTest.java
@@ -0,0 +1,40 @@
+package Selenium;
+
+import com.google.common.collect.ImmutableList;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v109.network.Network;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+import java.util.Optional;
+
+public class NetworkBlockRequestTest {
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*"); //https://github.com/SeleniumHQ/selenium/issues/11750
+        ChromeDriver driver = new ChromeDriver(options); //use ChromeDriver, not WebDriver to access Chrome DevTools Protocol
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+
+        //block loading unwanted files
+        devTools.send(Network.setBlockedURLs(ImmutableList.of("*.jpg", "*.css")));
+
+        long startTime = System.currentTimeMillis();
+        driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+        driver.findElement(By.linkText("Browse Products")).click();
+        driver.findElement(By.linkText("Selenium")).click();
+        driver.findElement(By.cssSelector(".add-to-cart")).click();
+        System.out.println(driver.findElement(By.cssSelector("p")).getText());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
